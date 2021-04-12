41d55b6eb6e57915de3ee97cebad19252ab40620
장상옥 <jso46034@naver.com>
2021-04-13 01:12:38 +0900

    @ModelAttribute 이용하여 Class 명시 study

---
+++ mvc_Ex01/src/main/java/com/java/ex/StudentInformation.java
@@ -0,0 +1,18 @@
+package com.java.ex;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class StudentInformation {
+
+	public String name;
+
+	public String age;
+
+	public String gradeNum;
+
+	public String classNum;
+
+}
